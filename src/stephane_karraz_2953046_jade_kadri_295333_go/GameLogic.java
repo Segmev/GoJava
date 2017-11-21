68272c15f283299f5e92f9eac8a97b50a04a1057
Stéphane Karraz <steffkarraz@hotmail.com>
2017-11-21 12:52:29 +0000

    Game logic started

---
+++ src/stephane_karraz_2953046_jade_kadri_295333_go/GameLogic.java
@@ -0,0 +1,33 @@
+package stephane_karraz_2953046_jade_kadri_295333_go;
+
+public class GameLogic {
+
+    private int[][]     board;
+
+    public void         init(int nbX, int nbY) {
+
+    }
+
+    public int[][]      getBoard() {
+        return board;
+    }
+
+    public boolean      playPos(int x, int y) {
+        return false;
+    }
+
+    public boolean      isGameEnded() {
+        return false;
+    }
+}
+
+
+/*
+    play(X, Y) {
+        if Board[X][Y] != EMPTY && C
+            return FALSE
+
+        if
+
+    }
+*/
